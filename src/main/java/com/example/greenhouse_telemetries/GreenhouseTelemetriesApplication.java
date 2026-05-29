11d2f97f67ed33903ca54f0c6f6ab786e2d19aa0
JavaToster <kamil.gizatullin.03@gmail.com>
2026-05-29 11:56:24 +0300

    First commit for telemetry microservice

---
+++ src/main/java/com/example/greenhouse_telemetries/GreenhouseTelemetriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenhouse_telemetries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseTelemetriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenhouseTelemetriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greenhouse_telemetries/GreenhouseTelemetriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenhouse_telemetries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhouseTelemetriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
